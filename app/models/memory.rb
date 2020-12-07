9dcd627a7042f41a5e6e6547b5081a7339b57250
yamazaki-shugo <yamaz1209876@icloud.com>
2020-12-07 17:07:58 +0900

    Active Strage導入

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -1,2 +1,7 @@
 class Memory < ApplicationRecord
+    belongs_to :user
+    has_one_attached :image
+
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to_active_hash :prefecture
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :momories
 
-         
   validates :nickname, presence: true, 
                        length: {maximum: 8}
   validates :birthday, presence: true
