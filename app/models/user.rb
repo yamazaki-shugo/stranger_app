2c82f7bfcc616cc8214ed97736871671d82ed9c0
yamazaki-shugo <yamaz1209876@icloud.com>
2020-12-07 17:01:16 +0900

    prefectureテーブル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
+         
   validates :nickname, presence: true, 
                        length: {maximum: 8}
   validates :birthday, presence: true
