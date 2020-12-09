df020efdabb3f1b21a480cf5e433c0273aaf4ad4
yamazaki-shugo <yamaz1209876@icloud.com>
2020-12-09 16:42:37 +0900

    バリデーション設定

--- app/models/record.rb
+++ app/models/record.rb
@@ -5,5 +5,11 @@ class Record < ApplicationRecord
     extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to_active_hash :prefecture
 
-    
+    with_options presence: true do 
+        validates :title, length: { maximum: 30 }
+        validates :content, length: { maximum: 660 }
+        validates :place, length: { maximum: 23 }
+        validates :prefecture_id, numericality: { other_than: 1 }
+        validates :image
+    end
 end
